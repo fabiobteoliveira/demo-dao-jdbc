537adfa3a8306322224ab8c2fc73869f96f4d588
Fabio Oliveira <fabiobteoliveira@gmail.com>
2020-11-05 17:39:56 +0000

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,6 +12,7 @@ public class Program {
 
         SellerDao sellerDao = DaoFactory.createSellerDao(); //Injeçao de independencia
 
+        System.out.println("==== TEST 1: seller findById ====");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
